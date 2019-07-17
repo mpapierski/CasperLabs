0c73c4e4cc63c37908f57a1f50769d6d6cc7affb
Michał Papierski <michal@casperlabs.io>
2019-07-17 11:40:43 -0400

    Reorganize.

--- execution-engine/comm/src/lib.rs
+++ execution-engine/comm/src/lib.rs
@@ -19,6 +19,16 @@ use jni::sys::{jbyteArray, jint, jlong, jobject, jobjectArray, jstring};
 use jni::JNIEnv;
 
 #[allow(non_snake_case)]
+pub extern "system" fn Java_ExecutionEngine_exec(_env: JNIEnv, _class: JClass) -> jobject {
+    unimplemented!();
+}
+
+#[allow(non_snake_case)]
+pub extern "system" fn Java_ExecutionEngine_commit(_env: JNIEnv, _class: JClass) -> jstring {
+    unimplemented!();
+}
+
+#[allow(non_snake_case)]
 pub extern "system" fn Java_ExecutionEngine_query(
     _env: JNIEnv,
     _class: JClass,
@@ -26,16 +36,6 @@ pub extern "system" fn Java_ExecutionEngine_query(
     // io.casperlabs.casper.consensus.state.Key base_key = 2;
     // repeated string path = 3;
 ) -> jobject {
-    unimplemented!();
-}
-
-#[allow(non_snake_case)]
-pub extern "system" fn Java_ExecutionEngine_exec(_env: JNIEnv, _class: JClass) -> jobject {
-    unimplemented!();
-}
-
-#[allow(non_snake_case)]
-pub extern "system" fn Java_ExecutionEngine_commit(_env: JNIEnv, _class: JClass) -> jstring {
     unimplemented!();
 }
 
