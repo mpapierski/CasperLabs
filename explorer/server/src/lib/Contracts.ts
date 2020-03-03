7887e5b02fc43161d4e6823450ff3c4ce2da8bd5
Michał Papierski <michal@casperlabs.io>
2020-03-03 18:10:37 +0100

    EE-911: Add support for new public key ABI.

--- explorer/server/src/lib/Contracts.ts
+++ explorer/server/src/lib/Contracts.ts
@@ -1,10 +1,19 @@
 import { Deploy } from "casperlabs-grpc/io/casperlabs/casper/consensus/consensus_pb";
 import { Args, ByteArray } from "casperlabs-sdk";
 
+const Ed25519Tag: number = 0;
+
+function toBytesPublicKey(publicKey: ByteArray): ByteArray {
+  const data = new Uint8Array(publicKey.length + 1);
+  data.set([Ed25519Tag]);
+  data.set(publicKey, 1);
+  return data;
+}
+
 export class Faucet {
   public static args(accountPublicKey: ByteArray, amount: bigint): Deploy.Arg[] {
     return Args.Args(
-      ["account", Args.BytesValue(accountPublicKey)],
+      ["account", Args.BytesValue(toBytesPublicKey(accountPublicKey))],
       ["amount", Args.BigIntValue(amount)]
     );
   }
