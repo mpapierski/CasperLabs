54dcb95d35c5ee12e0fc08164aef7fece72cdc76
Michał Papierski <michal@casperlabs.io>
2020-04-08 17:32:21 +0200

    EE-350: Update scala error handling.

--- casper/src/main/scala/io/casperlabs/casper/util/execengine/utils.scala
+++ casper/src/main/scala/io/casperlabs/casper/util/execengine/utils.scala
@@ -2,14 +2,22 @@ package io.casperlabs.casper.util.execengine
 
 import cats.Show
 import io.casperlabs.ipc.DeployError
-import io.casperlabs.ipc.DeployError.Value.{Empty, ExecError, GasError}
+import io.casperlabs.ipc.DeployError.Value.{
+  Empty,
+  ExecError,
+  GasError,
+  MissingSystemContract,
+  StorageError
+}
 
 object utils {
   implicit val deployErrorsShow: Show[DeployError] = Show.show {
     _.value match {
-      case Empty                                          => ""
-      case GasError(DeployError.OutOfGasError())          => "OutOfGas"
-      case ExecError(DeployError.ExecutionError(message)) => message
+      case Empty                                                             => ""
+      case GasError(DeployError.OutOfGasError())                             => "OutOfGas"
+      case ExecError(DeployError.ExecutionError(message))                    => message
+      case MissingSystemContract(DeployError.MissingSystemContract(message)) => message
+      case StorageError(DeployError.StorageError(message))                   => message
     }
   }
 }
